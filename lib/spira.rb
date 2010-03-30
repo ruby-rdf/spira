096a63536981a7b36f4bb723186c9531cd7b721e
Ben Lavender <blavender@gmail.com>
2010-03-30 23:23:34 +0200

    Update Spira.add_repository to optionally allow sending an instantiated repository instead of the options to create one

--- lib/spira.rb
+++ lib/spira.rb
@@ -6,7 +6,12 @@ module Spira
   Thread.current[:spira][:repositories] = {}
 
   def add_repository(name, klass, *args)
-    Thread.current[:spira][:repositories][name] = klass.new(*args)
+    Thread.current[:spira][:repositories][name] = case klass
+      when RDF::Repository
+        klass
+      else
+        klass.new(*args)
+     end
   end
   alias_method :add_repository!, :add_repository
   module_function :add_repository, :add_repository!
