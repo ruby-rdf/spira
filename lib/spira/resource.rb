c4404473d746552f0fd3830573b19b72a2c0e09c
Ben Lavender <blavender@gmail.com>
2010-05-20 16:06:07 +0200

    Add yardocs to spira/resource.rb

--- lib/spira/resource.rb
+++ lib/spira/resource.rb
@@ -1,4 +1,37 @@
 module Spira
+
+  ##
+  # Spira::Resource is the main interface to Spira.  Classes and modules
+  # include Spira::Resource to create projections of RDF data as a class.  For
+  # an overview, see the {file:README}.
+  #
+  # Projections are a mapping of RDF predicates to fields.
+  #
+  #     class Person
+  #       include Spira::Resource
+  #
+  #       property :name, :predicate => FOAF.name
+  #       property :age, :predicate => FOAF.age, :type => Integer
+  #     end
+  #
+  #     RDF::URI('http://example.org/people/bob').as(Person) #=> <#Person @uri=http://example.org/people/bob>
+  #
+  # Spira resources include the RDF namespace, and can thus reference all of
+  # the default RDF.rb vocabularies without the RDF:: prefix:
+  #
+  #     property :name, :predicate => FOAF.name
+  #
+  # The Spira::Resource documentation is broken into several parts, vaguely
+  # related by functionality:
+  #  * {Spira::Resource::DSL} contains methods used during the declaration of a class or module
+  #  * {Spira::Resource::ClassMethods} contains class methods for use by declared classes
+  #  * {Spira::Resource::InstanceMethods} contains methods for use by instances of Spira resource classes
+  #  * {Spira::Resource::Validations} contains some default validation functions
+  #
+  # @see Spira::Resource::DSL
+  # @see Spira::Resource::ClassMethods
+  # @see Spira::Resource::InstanceMethods
+  # @see Spira::Resource::Validations
   module Resource
  
     autoload :DSL,              'spira/resource/dsl'
@@ -6,6 +39,11 @@ module Spira
     autoload :InstanceMethods,  'spira/resource/instance_methods'
     autoload :Validations,      'spira/resource/validations'
 
+    ##
+    # When a child class includes Spira::Resource, this does the magic to make
+    # it a Spira resource.
+    #
+    # @private
     def self.included(child)
       # Don't do inclusion work twice.  Checking for the properties accessor is
       # a proxy for a proper check to see if this is a resource already.  Ruby
