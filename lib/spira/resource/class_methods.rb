f4a43db3e4380e7413b7512fa34df905bc7d7c8c
Ben Lavender <blavender@gmail.com>
2010-05-20 19:54:56 +0200

    Document spira/resource/class_methods.rb

--- lib/spira/resource/class_methods.rb
+++ lib/spira/resource/class_methods.rb
@@ -1,14 +1,21 @@
 module Spira
  module Resource
 
-   # This module contains all class methods available to a Spira::Resource class
+   ##
+   # This module contains all class methods available to a declared Spira::Resource class.
+   # {Spira::Resource} contains more information about Spira resources.
    #
-   #
+   # @see Spira::Resource
+   # @see Spira::Resource::InstanceMethods
+   # @see Spira::Resource::DSL
     module ClassMethods
-      def repository=(repo)
-        @repository = repo
-      end
 
+      ##
+      # The current repository for this class
+      # 
+      # @param  [RDF::Repository] repo The repository
+      # @return [Void]
+      # @private
       def repository
         case @repository_name
           when nil
@@ -18,6 +25,34 @@ module Spira
         end
       end
 
+      ##
+      # Create a new projection instance of this class for the given URI.  If a
+      # class has a base_uri given, and the argument is not an `RDF::URI`, the
+      # given identifier will be appended to the base URI.
+      #
+      # Spira does not have 'find' or 'create' functions.  As RDF identifiers
+      # are globally unique, they all simply 'are'.
+      #
+      # On calling `for`, a new instance is created for the given URI.  The
+      # first time access is attempted on a field, the repository will be
+      # queried for existing attributes, which will be used for the given URI.
+      # Underlying repositories are not accessed at the time of calling `for`.
+      # 
+      # A class with a base URI may still be projected for any URI, whether or
+      # not it uses the given resource class' base URI.
+      #
+      # @raise [TypeError] if an RDF type is given in the attributes and one is
+      # given in the attributes.  
+      # @raise [ArgumentError] if a non-URI is given and the class does not
+      # have a base URI.  
+      # @overload for(uri, attributes = {})
+      #   @param [RDF::URI] uri The URI to create an instance for
+      #   @param [Hash{Symbol => Any}] attributes Initial attributes
+      # @overload for(identifier, attributes = {})
+      #   @param [Any] uri The identifier to append to the base URI for this class
+      #   @param [Hash{Symbol => Any}] attributes Initial attributes
+      # @return  [Spira::Resource] The newly created instance
+      # @see http://rdf.rubyforge.org/RDF/URI.html
       def for(identifier, attributes = {})
         if !self.type.nil? && attributes[:type]
           raise TypeError, "#{self} has an RDF type, #{self.type}, and cannot accept one as an argument."
@@ -29,9 +64,10 @@ module Spira
       ##
       # Creates a URI based on a base_uri and string or URI
       #
-      # @param [Any] Identifier
+      # @param  [Any] Identifier
       # @return [RDF::URI]
-      # @raises [ArgumentError] If this class cannot create an identifier from the given string
+      # @raise  [ArgumentError] If this class cannot create an identifier from the given string
+      # @see http://rdf.rubyforge.org/RDF/URI.html
       def uri_for(identifier)
         case identifier
           when RDF::URI
@@ -47,16 +83,33 @@ module Spira
         end
       end
 
+
+      ##
+      # The number of URIs projectable as a given class in the repository.
+      # This method is only valid for classes which declare a `type` with the
+      # `type` method in the DSL.
+      #
+      # @raise  [TypeError] if the resource class does not have an RDF type declared
+      # @return [Integer] the count
+      # @see Spira::Resource::DSL
       def count
         raise TypeError, "Cannot count a #{self} without a reference type URI." if @type.nil?
         result = repository.query(:predicate => RDF.type, :object => @type)
         result.count
       end
 
+      ##
+      # Returns true if the given property is a has_many property, false otherwise
+      #
+      # @return [true, false]
       def is_list?(property)
         @lists.has_key?(property)
       end
 
+      ##
+      # Handling inheritance
+      #
+      # @private
       def inherited(child)
         child.instance_eval do
           include Spira::Resource
@@ -79,14 +132,18 @@ module Spira
         end
       end
 
+      ## 
+      # Handling module inclusions
+      #
+      # @private
       def included(child)
         inherited(child)
       end
 
       ## 
-      # The list of validation functions for this aspect
+      # The list of validation functions for this projection
       #
-      # @return [Array[Symbol]]
+      # @return [Array<Symbol>]
       def validators
         @validators ||= []
       end
