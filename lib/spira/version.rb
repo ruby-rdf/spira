d7c4f377cde661e16b2e42afdc408e4af4885b67
Ben Lavender <blavender@gmail.com>
2010-05-20 20:42:10 +0200

    Add a Spira::VERSION

---
+++ lib/spira/version.rb
@@ -0,0 +1,23 @@
+module Spira
+  module VERSION
+    MAJOR = 0
+    MINOR = 0
+    TINY  = 1
+    EXTRA = nil
+
+    STRING = [MAJOR, MINOR, TINY].join('.')
+    STRING << ".#{EXTRA}" if EXTRA
+
+    ##
+    # @return [String]
+    def self.to_s() STRING end
+
+    ##
+    # @return [String]
+    def self.to_str() STRING end
+
+    ##
+    # @return [Array(Integer, Integer, Integer)]
+    def self.to_a() [MAJOR, MINOR, TINY] end
+  end
+end
