6740156e0052eeb3d8b023026ec13ccdbc89863e
Ben Lavender <blavender@gmail.com>
2010-05-20 23:56:12 +0200

    Reorganize loading in spec_helper to prevent some bugs when testing individual files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,12 @@
-require 'spira'
-require 'rdf/isomorphic'
-
 $:.unshift(File.join(File.dirname(__FILE__),'..','lib'))
 $:.unshift(File.join(File.dirname(__FILE__),'fixtures',''))
-$:.unshift(File.join(File.dirname(__FILE__),'..','..','rdf-spec','lib'))
-$:.unshift(File.join(File.dirname(__FILE__),'..','..','rdf','lib'))
+#$:.unshift(File.join(File.dirname(__FILE__),'..','..','rdf-spec','lib'))
+#$:.unshift(File.join(File.dirname(__FILE__),'..','..','rdf','lib'))
+#$:.unshift(File.join(File.dirname(__FILE__),'..','..','rdf-isomorphic','lib'))
 require 'spira'
 require 'rdf/spec/enumerable'
 require 'rdf/spec'
+require 'rdf/isomorphic'
 
 def fixture(filename)
   File.join(File.dirname(__FILE__),'fixtures',filename)
