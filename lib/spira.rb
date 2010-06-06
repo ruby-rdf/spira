dd234f1314cb1c1931477449657892ae134d6ff8
Ben Lavender <blavender@gmail.com>
2010-06-07 01:12:13 +0200

    Implement RDF::Node#as

--- lib/spira.rb
+++ lib/spira.rb
@@ -133,4 +133,20 @@ module RDF
       klass.for(self, *args)
     end
   end
+
+  class Node
+    ##
+    # Create a projection of this Node as the given Spira::Resource class.
+    # Equivalent to `klass.for(self, *args)`
+    #
+    # @example Instantiating a blank node as a Spira Resource
+    #     RDF::Node.new.as(Person)
+    # @param [Class] klass
+    # @param [*Any] args Any arguments to pass to klass.for
+    # @return [Klass] An instance of klass
+    def as(klass, *args)
+      raise ArgumentError, "#{klass} is not a Spira resource" unless klass.is_a?(Class) && klass.ancestors.include?(Spira::Resource)
+      klass.for(self, *args)
+    end
+  end
 end
