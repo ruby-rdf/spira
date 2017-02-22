5b7b91ef321e628a2bdc0ae625d97f0442882dfc
Aymeric Brisse <aymeric.brisse@perfect-memory.com>
2017-02-22 18:33:03 +0100

    Add support for literal type XSD.anyURI

---
+++ lib/spira/types/anyURI.rb
@@ -0,0 +1,27 @@
+module Spira::Types
+
+  ##
+  # A {Spira::Type} for URI values.  Values will be associated with the
+  # `XSD.anyURI` type with no language code.
+  #
+  # A {Spira::Resource} property can reference this type as
+  # `Spira::Types::AnyURI`, `AnyURI`, or `XSD.anyURI`.
+  #
+  # @see Spira::Type
+  # @see http://rdf.rubyforge.org/RDF/Literal.html
+  class AnyURI
+
+    include Spira::Type
+
+    def self.unserialize(value)
+      value.object
+    end
+
+    def self.serialize(value)
+      RDF::Literal.new(value, :datatype => XSD.anyURI)
+    end
+
+    register_alias XSD.anyURI
+
+  end
+end
