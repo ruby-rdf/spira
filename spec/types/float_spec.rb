bbd3a7d65c1b449f1423d48a337efb0891ad2e2c
Aymeric Brisse <aymeric.brisse@perfect-memory.com>
2017-03-01 11:23:46 +0100

    Separate Double & Float types

--- spec/types/float_spec.rb
+++ spec/types/float_spec.rb
@@ -7,16 +7,14 @@ describe Spira::Types::Float do
       serialized = Spira::Types::Float.serialize(5.0)
       expect(serialized).to be_a RDF::Literal
       expect(serialized).to have_datatype
-      expect(serialized.datatype).to eql RDF::XSD.double
-      expect(serialized).to eql RDF::Literal.new(5.0)
+      expect(serialized.datatype).to eql RDF::XSD.float
     end
 
     it "should serialize integers to XSD floats" do
       serialized = Spira::Types::Float.serialize(5)
       expect(serialized).to be_a RDF::Literal
       expect(serialized).to have_datatype
-      expect(serialized.datatype).to eql RDF::XSD.double
-      expect(serialized).to eql RDF::Literal.new(5.0)
+      expect(serialized.datatype).to eql RDF::XSD.float
     end
 
   end
@@ -24,12 +22,6 @@ describe Spira::Types::Float do
   context "when unserializing" do
     it "should unserialize XSD floats to floats" do
       value = Spira::Types::Float.unserialize(RDF::Literal.new(5, :datatype => RDF::XSD.float))
-      expect(value).to be_a Float
-      expect(value).to eql 5.0
-    end
-
-    it "should unserialize XSD doubles to floats" do
-      value = Spira::Types::Float.unserialize(RDF::Literal.new(5, :datatype => RDF::XSD.double))
       expect(value).to be_a Float
       expect(value).to eql 5.0
     end
